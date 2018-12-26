e6b36197e7258e6c53d2a0edab5744826ce3adaf
赵虎 <zhhnoah@gmail.com>
2018-12-26 11:37:04 +0800

    Updates

---
+++ precache-manifest.0e069b5f513bc17dcced6e9f0bb41bf1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/develop-center/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "c407224e2411db89864d",
+    "url": "/develop-center/static/css/main.4260c0f1.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/develop-center/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "9f8a3f5b590de075d455",
+    "url": "/develop-center/static/js/1.9f8a3f5b.chunk.js"
+  },
+  {
+    "revision": "9d941d53afd8b76b46f7",
+    "url": "/develop-center/static/js/runtime~main.9d941d53.js"
+  },
+  {
+    "revision": "c407224e2411db89864d",
+    "url": "/develop-center/static/js/main.c407224e.chunk.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/develop-center/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/develop-center/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/develop-center/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "9f8a3f5b590de075d455",
+    "url": "/develop-center/static/css/1.3789e562.chunk.css"
+  },
+  {
+    "revision": "6da242d791a86b9593b5cd9ada3f1a56",
+    "url": "/develop-center/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f2b4ffced6a8e1112f4aabb8c3b1c1c.js"
+  "/develop-center/precache-manifest.0e069b5f513bc17dcced6e9f0bb41bf1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/develop-center/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
